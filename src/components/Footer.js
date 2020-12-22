9b4d28cc587568a9561accb2537bbbd236f3b7dc
mannycolon <colonmanuel7@gmail.com>
2020-12-21 21:10:05 -0500

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(props) {
+  return (
+    <footer className='border border-solid border-gray-200 h-24 flex items-center justify-center'>
+      {'I`m the footer'}
+    </footer>
+  )
+}
