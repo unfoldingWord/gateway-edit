4ce9f2fa8b7858b2390898b4619d165fac8cfee7
mannycolon <colonmanuel7@gmail.com>
2021-01-07 19:35:53 -0500

    Header & Footer fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import NetlifyBadge from '@components/NetlifyBadge'
 
 export default function Footer(props) {
   return (
-    <footer className='flex justify-center items-center h-28 w-screen bg-primary border  border-solid  border-gray-200 p-10'>
+    <footer className='flex justify-center items-center h-28 w-screen bg-primary border border-solid border-gray-200'>
       <NetlifyBadge />
     </footer>
   )
