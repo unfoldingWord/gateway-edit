b76542dc20d159cacb6e50186f7c4d56e4e5f81d
mannycolon <colonmanuel7@gmail.com>
2021-01-24 18:13:15 -0500

    HELP_DESK_EMAIL

--- src/common/sendFeedback.js
+++ src/common/sendFeedback.js
@@ -13,7 +13,7 @@ export default async function sendFeedback({ name, email, message, category }) {
   }
 
   const msg = {
-    // to: process.env.HELP_DESK_EMAIL,
+    to: process.env.HELP_DESK_EMAIL,
     to: 'colonmanuel7@gmail.com',
     from: email,
     subject: `Create App v2: ${category}`,
