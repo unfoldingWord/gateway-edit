292184bb569f1fe2043a6f61846404e040a263c6
Bruce McLean <mcleanb@photo-nomad.net>
2021-03-23 08:20:27 -0400

    fix scripts to remove extra counter

--- scripts/update-build.sh
+++ scripts/update-build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 build_hash_dir=public/build_number
-build_hash=`cat "$build_hash_dir"`
 
-build_hash_split=(${build_hash//-/ })
-build_number=(${build_hash_split[0]})
 build_commit_hash=$(git rev-parse --short HEAD)
 
-echo "$build_number-$build_commit_hash" > "$build_hash_dir"
-echo "Setting current build number: $build_number-$build_commit_hash"
+echo "$build_commit_hash" > "$build_hash_dir"
+echo "Setting current build hash: $build_commit_hash"
