ef317fd81c3371ae2b81470fac5f441c2d037d49
Bruce McLean <mcleanb@photo-nomad.net>
2021-08-09 09:04:14 -0400

    working on posting to zulip

--- scripts/zulip-notification/index.js
+++ scripts/zulip-notification/index.js
@@ -1,17 +1,33 @@
 // index.js
 
-const got = require('got')
+const axios = require('axios')
 
 module.exports = {
-  onPreBuild: ({ packageJson, netlifyConfig }) => {
+  onPreBuild: async ({ packageJson, netlifyConfig }) => {
     console.log('Hello world from onPreBuild event!')
     console.log({ netlifyConfig })
-    console.log({ got })
     // console.log({ packageJson })
     // console.log(process.env)
     const token = process.env.ZULIP_TOKEN
     const name = packageJson.name
     const version = packageJson.version
     console.log(`Building ${name} ${version}, token length = ${token.length}`)
-  },
+
+    try {
+      await axios.post('https://unfoldingword.zulipchat.com/api/v1/messages',
+        {
+          type:     'stream',
+          to:       'SOFTWARE - UR/github',
+          subject:  'netlify testing',
+          content:  'This is another test',
+        },
+        {
+          headers: {
+            'Authorization': `Basic netlify-bot@unfoldingword.zulipchat.com:${token}`,
+          },
+        })
+    } catch (error) {
+      console.error('error Sending Zulip notification', error)
+    }
+  }
 }
