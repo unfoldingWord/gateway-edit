1103bbd159649a4b369861a07ba6d3825181fe48
Bruce McLean <mcleanb@photo-nomad.net>
2021-08-09 08:39:33 -0400

    working on plugin

--- scripts/zulip-notification/index.js
+++ scripts/zulip-notification/index.js
@@ -1,14 +1,17 @@
 // index.js
 
+const https = require('https')
+
 module.exports = {
   onPreBuild: ({ packageJson, netlifyConfig }) => {
     console.log('Hello world from onPreBuild event!')
     console.log({ netlifyConfig })
+    console.log({ https })
     // console.log({ packageJson })
     // console.log(process.env)
     const token = process.env.ZULIP_TOKEN
     const name = packageJson.name
     const version = packageJson.version
-    console.log(`Building ${name} ${version}, token length = ${token?.length}`)
+    console.log(`Building ${name} ${version}, token length = ${token.length}`)
   },
 }
