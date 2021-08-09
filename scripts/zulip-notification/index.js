3077be2e37971fbfdb1ca3f1d316c6949937d4fb
Bruce McLean <mcleanb@photo-nomad.net>
2021-08-09 08:25:57 -0400

    working on plugin

--- scripts/zulip-notification/index.js
+++ scripts/zulip-notification/index.js
@@ -4,7 +4,11 @@ module.exports = {
   onPreBuild: ({ packageJson, netlifyConfig }) => {
     console.log('Hello world from onPreBuild event!')
     console.log({ netlifyConfig })
-    console.log({ packageJson })
-    console.log(process.env)
+    // console.log({ packageJson })
+    // console.log(process.env)
+    const token = process.env.ZULIP_TOKEN
+    const name = packageJson.name
+    const version = packageJson.version
+    console.log(`Building ${name} ${version}, token length = ${token}`)
   },
 }
