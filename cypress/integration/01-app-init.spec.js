934bbf65800c02c1170a3e5f2acef2d0118ce086
Manny Colon <colonmanuel7@gmail.com>
2021-10-19 16:18:38 -0400

    Fixed e2e test

--- cypress/integration/01-app-init.spec.js
+++ cypress/integration/01-app-init.spec.js
@@ -9,6 +9,8 @@ describe('Login', () => {
     cy.get('input[type="password"]').type(Cypress.env('TEST_PASSWORD'))
     cy.get('[data-test="submit-button"]').click()
 
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
     cy.get('h3').contains('Account Setup').should('be.visible')
     cy.contains('Choose your Organization and Primary Language')
 
