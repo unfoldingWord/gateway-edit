6ab61cbdc94a7960b102a23557871f1b2a1a0150
Bruce McLean <mcleanb@photo-nomad.net>
2021-10-25 14:22:39 -0400

    working on jest testing

--- src/utils/__tests__/lexiconHelpers.spec.js
+++ src/utils/__tests__/lexiconHelpers.spec.js
@@ -1,12 +1,36 @@
 /// <reference types="jest" />
 
 import { getOriginalLanguageStr } from '../lexiconHelpers'
+const GREEK = 'Greek'
+const HEBREW = 'Hebrew'
 
 describe('persistence.getOriginalLanguageStr', () => {
-  it('should save the state to local storage', async () => {
+  it('isNT true should return Greek',() => {
     // when
     const isNT = true
-    const expected = 'Greek'
+    const expected = GREEK
+
+    // then
+    const results = getOriginalLanguageStr(isNT)
+
+    expect(results).toEqual(expected)
+  })
+
+  it('isNT false should return Hebrew',() => {
+    // when
+    const isNT = false
+    const expected = HEBREW
+
+    // then
+    const results = getOriginalLanguageStr(isNT)
+
+    expect(results).toEqual(expected)
+  })
+
+  it('isNT falsy should return Hebrew',() => {
+    // when
+    const isNT = null
+    const expected = HEBREW
 
     // then
     const results = getOriginalLanguageStr(isNT)
