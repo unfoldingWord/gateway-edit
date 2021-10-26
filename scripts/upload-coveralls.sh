ee354b1b240346899ced1a1eda7654943cfd3d22
Bruce McLean <mcleanb@photo-nomad.net>
2021-10-26 16:36:25 -0400

    working on unit tests

--- scripts/upload-coveralls.sh
+++ scripts/upload-coveralls.sh
@@ -7,20 +7,22 @@ if [[ -z "${COVERALLS_REPO_TOKEN}" ]]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-if [[ -z "${COVERALLS_REPO_TOKEN}" ]]; then
-  echo "COVERALLS_REPO_TOKEN could not be found"
-fi
-
 if [[ -z "${CYPRESS_TEST_USERNAME}" ]]; then
   echo "CYPRESS_TEST_USERNAME found: ${CYPRESS_TEST_USERNAME}"
 fi
 
-if [[ -z "${env.CYPRESS_TEST_USERNAME}" ]]; then
-  echo "env.CYPRESS_TEST_USERNAME found: ${CYPRESS_TEST_USERNAME}"
+if [[ -z "${env.COVERALLS_REPO_TOKEN}" ]]; then
+  echo "env.COVERALLS_REPO_TOKEN found: ${env.COVERALLS_REPO_TOKEN}"
+  export COVERALLS_REPO_TOKEN=${env.COVERALLS_REPO_TOKEN}
 fi
 
 if [[ -z "${secret.CYPRESS_TEST_USERNAME}" ]]; then
-  echo "secret.CYPRESS_TEST_USERNAME found: ${CYPRESS_TEST_USERNAME}"
+  echo "secret.CYPRESS_TEST_USERNAME found: ${secret.CYPRESS_TEST_USERNAME}"
+  export COVERALLS_REPO_TOKEN=${secret.COVERALLS_REPO_TOKEN}
+fi
+
+if [[ -z "${COVERALLS_REPO_TOKEN}" ]]; then
+  echo "COVERALLS_REPO_TOKEN could not be found"
 fi
 
 yarn upload
