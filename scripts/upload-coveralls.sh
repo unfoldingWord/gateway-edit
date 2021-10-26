ba9077e322561a440e4510b4d9ece00f2a12d7a6
Bruce McLean <mcleanb@photo-nomad.net>
2021-10-26 18:55:13 -0400

    working on unit tests - coverage

--- scripts/upload-coveralls.sh
+++ scripts/upload-coveralls.sh
@@ -11,16 +11,6 @@ if [[ -z "${CYPRESS_TEST_USERNAME}" ]]; then
   echo "CYPRESS_TEST_USERNAME found: ${CYPRESS_TEST_USERNAME}"
 fi
 
-if [[ -z "${env.COVERALLS_REPO_TOKEN}" ]]; then
-  echo "env.COVERALLS_REPO_TOKEN found: ${env.COVERALLS_REPO_TOKEN}"
-  export COVERALLS_REPO_TOKEN=${env.COVERALLS_REPO_TOKEN}
-fi
-
-if [[ -z "${secret.CYPRESS_TEST_USERNAME}" ]]; then
-  echo "secret.CYPRESS_TEST_USERNAME found: ${secret.CYPRESS_TEST_USERNAME}"
-  export COVERALLS_REPO_TOKEN=${secret.COVERALLS_REPO_TOKEN}
-fi
-
 if [[ -z "${COVERALLS_REPO_TOKEN}" ]]; then
   echo "COVERALLS_REPO_TOKEN could not be found"
 fi
