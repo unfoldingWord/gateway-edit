daffb6fb6a67f6b7e39cef4fe3c01882bc494970
Manny Colon <colonmanuel7@gmail.com>
2021-11-16 09:04:03 -0500

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,15 @@ module.exports = {
   'coveragePathIgnorePatterns': [
     '/node_modules/',
     '/.yalc/',
+    'scripts',
+    'cypress',
   ],
   'testPathIgnorePatterns': [
     '/node_modules/',
     'components',
     'cypress',
     '/.yalc/',
+    'scripts',
   ],
   'testMatch': [
     '**/?(*.)+(spec|test).[jt]s?(x)',
