d593adf0716997dbfc0077cc9a5c44f78ee41c20
Manny Colon <colonmanuel7@gmail.com>
2022-01-31 15:48:07 -0500

    Updated secondary theme main color

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@material-ui/core/styles'
-import { red } from '@material-ui/core/colors'
+import { red, pink } from '@material-ui/core/colors'
 
 // Create a theme instance.
 const theme = createTheme({
@@ -9,7 +9,7 @@ const theme = createTheme({
       contrastText: '#fff',
     },
     secondary: {
-      main: '#19857b',
+      main: pink.A200,
       contrastText: '#000',
     },
     error: { main: red.A400 },
