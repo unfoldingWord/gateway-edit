ad37b6442d2bc6eedd387462fe4aeeb337fe62ae
larsgson <4819617+larsgson@users.noreply.github.com>
2022-07-11 23:32:18 +0200

    All dependencies updated to latest versions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/canvas/))
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /canvas/ }))
 
     // Important: return the modified config
     return config
