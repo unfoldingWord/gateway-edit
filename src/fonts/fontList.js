05328ba109ab49d1295088de7ff5d10dbab53183
jincypjose <jincy370@gmail.com>
2023-04-12 18:11:52 +0530

    adding font selection

---
+++ src/fonts/fontList.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'Scheherazade New',
+    fontFamily: 'ScheherazadeNewW',
+    graphite: true,
+  },
+  {
+    name: 'Charis SIL',
+    fontFamily: 'CharisSILW',
+    graphite: true,
+  },
+  {
+    name: 'Abyssinica SIL',
+    fontFamily: 'AbyssinicaSILW',
+    graphite: true,
+  },
+  {
+    name: 'Andika',
+    fontFamily: 'AndikaW',
+    graphite: true,
+  },
+  {
+    name: 'Annapurna SIL',
+    fontFamily: 'AnnapurnaSILW',
+    graphite: true,
+  },
+  {
+    name: 'Awami Nastaliq',
+    fontFamily: 'AwamiNastaliqW',
+    graphite: true,
+  },
+  {
+    name: 'Doulos SIL',
+    fontFamily: 'DoulosSILW',
+    graphite: true,
+  },
+  {
+    name: 'Gentium Plus',
+    fontFamily: 'GentiumPlusW',
+    graphite: true,
+  },
+  {
+    name: 'Harmattan',
+    fontFamily: 'HarmattanW',
+    graphite: true,
+  },
+  {
+    name: 'Lateef',
+    fontFamily: 'LateefW',
+    graphite: true,
+  },
+  {
+    name: 'Padauk',
+    fontFamily: 'PadaukW',
+    graphite: true,
+  },
+  {
+    name: 'Noto',
+    fontFamily: 'NotoW',
+    graphite: false,
+  },
+]
