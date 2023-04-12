ac64e0ccb5749849beaba065cd95fdbb76dcbde8
jincypjose <66897554+jincypjose@users.noreply.github.com>
2023-04-12 19:17:22 +0530

    Feature jincy 399 (#403)
    * added git sync icons
    
    * connect merge status
    
    * revert some inadvertant changes
    
    * Added ability to resource and scripture cards to merge to/from master
    
    * added logging and fixed the merge status
    
    * updated the translation-help-rcl version
    
    * updated the version
    
    ---------
    
    Co-authored-by: blm <mcleanb@photo-nomad.net>
    Co-authored-by: kintsoogi <kintsoogii@proton.me>

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -11,11 +11,14 @@ import {
   MANIFEST_NOT_LOADED_ERROR,
 } from 'translation-helps-rcl'
 import { useEdit } from 'gitea-react-toolkit'
+import { MdUpdate, MdUpdateDisabled } from 'react-icons/md'
+import { FiShare } from 'react-icons/fi'
 import { getResourceErrorMessage } from 'single-scripture-rcl'
 import { getResourceMessage } from '@utils/resources'
 import { RESOURCE_HTTP_CONFIG, SERVER_MAX_WAIT_TIME_RETRY } from '@common/constants'
 import generateEditFilePath from '@utils/generateEditFilePath'
 import getSha from '@utils/getSha'
+import { IconButton } from '@mui/material'
 
 export default function ResourceCard({
   id,
@@ -79,12 +82,18 @@ export default function ResourceCard({
 
   const {
     state: {
-      listRef,
       contentRef,
+      listRef,
       usingUserBranch,
       workingResourceBranch,
+      mergeFromMaster,
+      mergeToMaster,
     },
-    actions: { startEdit },
+    actions: {
+      startEdit,
+      mergeFromMasterIntoUserBranch,
+      mergeToMasterFromUserBranch
+    },
   } = useUserBranch({
     owner,
     server,
@@ -237,6 +246,61 @@ export default function ResourceCard({
   const editableResources = ['tw', 'ta', 'tn', 'tq', 'twl']
   const editable = editableResources.includes(cardResourceId)
 
+  const needToMergeFromMaster = mergeFromMaster?.mergeNeeded
+  const mergeFromMasterHasConflicts = mergeFromMaster?.conflict
+  const mergeToMasterHasConflicts = mergeToMaster?.conflict
+
+  // eslint-disable-next-line no-nested-ternary
+  const mergeFromMasterTitle = mergeFromMasterHasConflicts ? 'Merge Conflicts for update from master' : (needToMergeFromMaster ? 'Update from master' : 'No merge conflicts for update with master')
+  // eslint-disable-next-line no-nested-ternary
+  const mergeFromMasterColor = mergeFromMasterHasConflicts ? 'black' : (needToMergeFromMaster ? 'black' : 'lightgray')
+  const mergeToMasterTitle = mergeToMasterHasConflicts ? 'Merge Conflicts for share with master' : 'No merge conflicts for share with master'
+  const mergeToMasterColor = mergeToMasterHasConflicts ? 'black' : 'black'
+
+  const onRenderToolbar = ({ items }) => {
+    const newItems = [...items]
+
+    if (mergeFromMaster) {
+      newItems.push(
+        <IconButton
+          className={classes.margin}
+          key='update-from-master'
+          onClick={mergeFromMasterIntoUserBranch}
+          title={mergeFromMasterTitle}
+          aria-label={mergeFromMasterTitle}
+          style={{ cursor: 'pointer' }}
+        >
+          {mergeFromMasterHasConflicts ?
+            <MdUpdateDisabled id='update-from-master-icon' color={mergeFromMasterColor} />
+            :
+            <MdUpdate id='update-from-master-icon' color={mergeFromMasterColor} />
+          }
+        </IconButton>
+      )
+    }
+
+    if (mergeToMaster) {
+      newItems.push(
+        <IconButton
+          className={classes.margin}
+          key='share-to-master'
+          onClick={mergeToMasterFromUserBranch}
+          title={mergeToMasterTitle}
+          aria-label={mergeToMasterTitle}
+          style={{ cursor: 'pointer' }}
+        >
+          {mergeToMasterHasConflicts ?
+            <MdUpdateDisabled id='share-to-master-icon' color={mergeToMasterColor} />
+            :
+            <FiShare id='share-to-master-icon' color={mergeToMasterColor} />
+          }
+        </IconButton>
+      )
+    }
+    return newItems
+  }
+
+
   return (
     <Card
       id={id}
@@ -262,6 +326,7 @@ export default function ResourceCard({
       hideMarkdownToggle={hideMarkdownToggle}
       showSaveChangesPrompt={showSaveChangesPrompt}
       onMinimize={onMinimize ? () => onMinimize(id) : null}
+      onRenderToolbar={onRenderToolbar}
     >
       <CardContent
         id={`${id}_content`}
