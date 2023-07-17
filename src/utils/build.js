2a6f4b3d53f34341e62d44e571f3a42b3461e286
blm <mcleanb@photo-nomad.net>
2023-07-17 12:16:07 -0400

    getting environment vars

--- src/utils/build.js
+++ src/utils/build.js
@@ -11,6 +11,8 @@ export function getCommitHash() {
 
   try {
     commitHash = process.env.NEXT_PUBLIC_BUILD_NUMBER
+    console.log('process.env.NEXT_PUBLIC_BUILD_CONTEXT:', process.env.NEXT_PUBLIC_BUILD_CONTEXT)
+    console.log('process.env.NEXT_PUBLIC_BUILD_BRANCH:', process.env.NEXT_PUBLIC_BUILD_BRANCH)
   } catch (e) {
     console.error(`getCommitHash() - could not get commit hash`, e)
   }
