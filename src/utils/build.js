28752e9e84f97e083579ea72a9646cbb05c01fbf
blm <mcleanb@photo-nomad.net>
2023-07-18 07:20:47 -0400

    cleanup

--- src/utils/build.js
+++ src/utils/build.js
@@ -11,8 +11,6 @@ export function getCommitHash() {
 
   try {
     commitHash = process.env.NEXT_PUBLIC_BUILD_NUMBER
-    console.log('process.env.NEXT_PUBLIC_BUILD_CONTEXT:', process.env.NEXT_PUBLIC_BUILD_CONTEXT)
-    console.log('process.env.NEXT_PUBLIC_BUILD_BRANCH:', process.env.NEXT_PUBLIC_BUILD_BRANCH)
   } catch (e) {
     console.error(`getCommitHash() - could not get commit hash`, e)
   }
