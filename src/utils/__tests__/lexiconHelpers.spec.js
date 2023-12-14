c772c10a53019dba413df95187bba688322bfbb5
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2023-12-14 12:40:59 -0500

    cleanup

--- src/utils/__tests__/lexiconHelpers.spec.js
+++ src/utils/__tests__/lexiconHelpers.spec.js
@@ -1,7 +1,5 @@
 /// <reference types="jest" />
 import { describe, it, expect } from 'vitest'
-// import '@testing-library/jest-dom'
-//import { render, screen } from '@testing-library/reacqt'
 
 import { getOriginalLanguageStr } from '../lexiconHelpers'
 const GREEK = 'Greek'
