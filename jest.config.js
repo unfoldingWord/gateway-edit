7c13dc71268f75df36225628330c6b70ded7ca4d
kintsoogi <kintsoogii@proton.me>
2024-04-29 16:02:48 -0500

    Upgraded to Next v12, React v18, and Scripture-Tsv v1.2.0

--- jest.config.js
+++ jest.config.js
@@ -1,37 +1,34 @@
 module.exports = {
-  'roots': ['<rootDir>'],
-  // 'transform': { '^.+\\.ts?$': 'ts-jest' },
-  'moduleFileExtensions': [
-    'js',
-    'ts',
-  ],
-  'coveragePathIgnorePatterns': [
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+  moduleFileExtensions: ['js', 'ts'],
+  coveragePathIgnorePatterns: [
     '/node_modules/',
     '/.yalc/',
     'scripts',
     'cypress',
   ],
-  'testPathIgnorePatterns': [
+  testPathIgnorePatterns: [
     '/node_modules/',
     'components',
     'cypress',
     '/.yalc/',
     'scripts',
   ],
-  'testMatch': [
-    '**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
-  'moduleNameMapper': {
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
     '^@common/(.*)$': '<rootDir>/src/common/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@context/(.*)$': '<rootDir>/src/context/$1',
     '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
   },
-  'collectCoverageFrom': [
+  collectCoverageFrom: [
     './src/**.{js,jsx,ts}',
     '!**/node_modules/**',
     '!**/vendor/**',
   ],
-  'coverageDirectory': 'jest-coverage',
+  coverageDirectory: 'jest-coverage',
 }
